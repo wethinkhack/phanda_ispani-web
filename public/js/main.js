a1d7855b618e527274cb1d121620a71b24e43061
Junior <jumabunjhb024@student.wethinkcode.co.za>
2024-10-05 09:22:45 +0200

    Remove Firebase files

--- public/js/main.js
+++
@@ -1,4 +0,0 @@
-// Mobile menu toggle implementation
-document.querySelector("#menu_toggle").addEventListener('click', function() {
-    document.querySelector('#nav_bar').classList.toggle('open');
-});
