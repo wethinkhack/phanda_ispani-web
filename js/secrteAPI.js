2a944c6988001cf9540e45a8a1567ece8532cdff
lloyd <drippyxlolo17@gmail.com>
2024-10-06 05:28:11 +0200

    changes to signin

---
+++ js/secrteAPI.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyByohDdD4gSJyjMbk--RTzDarS2jSQlN6U",
+    authDomain: "phanda-ispani-e5471.firebaseapp.com",
+    projectId: "phanda-ispani-e5471",
+    storageBucket: "phanda-ispani-e5471.appspot.com",
+    messagingSenderId: "405153328165",
+    appId: "1:405153328165:web:d249fef63e3481514723cc",
+    measurementId: "G-T074XCXM33"
+  };
+
+module.exports = firebaseConfig;
--- js/signUp.js
+++ js/signUp.js
@@ -1,16 +1,7 @@
 // Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyDMI6MED71-FeM57FLzx7p-Jc_83Q-bzCM",
-    authDomain: "ayobaproconnect.firebaseapp.com",
-    databaseURL: "https://ayobaproconnect-default-rtdb.firebaseio.com",
-    projectId: "ayobaproconnect",
-    storageBucket: "ayobaproconnect.appspot.com",
-    messagingSenderId: "457982210991",
-    appId: "1:457982210991:web:35858882b2b03e9267355a",
-    measurementId: "G-B46J0446E2"
-  };
-  //initialize firebase
-  firebase.initializeApp(firebaseConfig);
+import("secrteAPI");
+//initialize firebase
+firebase.initializeApp(firebaseConfig);
   
 //reference your database
 var bizdatabase = firebase.database().ref("form");
